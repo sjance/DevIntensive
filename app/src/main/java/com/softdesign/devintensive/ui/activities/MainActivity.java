2d8779338579b1b7a22e107a153d20615a3f489a
Alexander Tretyak <alexandertretjak@gmail.com>
2016-06-27 13:57:17 +0300

    Added Scrolling Deleted styles/v16

--- app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
+++ app/src/main/java/com/softdesign/devintensive/ui/activities/MainActivity.java
@@ -3,19 +3,25 @@ package com.softdesign.devintensive.ui.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.softdesign.devintensive.R;
 import com.softdesign.devintensive.utils.ConstantManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends BaseActivity implements View.OnClickListener {
 
     private static final String TAG = ConstantManager.TAG_PREFIX + "Main Activity";
+    private ImageView mCallImg;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d(TAG, "onCreate");
+
+        mCallImg = (ImageView)findViewById(R.id.call_img);
+        mCallImg.setOnClickListener(this);
 
         if (savedInstanceState == null) {
             // первый запуск activity
@@ -58,5 +64,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Log.d(TAG, "onRestart");
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.call_img:
+            showProgress();
+            break;
+        }
     }
 }
